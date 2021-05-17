92f379224c0541bbe6a89d961f3298f4c89e0bf5
jake-stewart <83528263+jake-stewart@users.noreply.github.com>
2021-05-17 22:31:05 +0800

    Update pygrid.py

--- pygrid.py
+++ pygrid.py
@@ -28,7 +28,7 @@ DELETING = 6
 class PyGrid:
     def __init__(self, n_rows=20, n_columns=20, width=0, height=0,
                  background_color=(255, 255, 255), grid_color=(50, 50, 50),
-                 grid_fade_start=40, grid_fade_end=8, grid_percentage=0.1,
+                 grid_fade_start=30, grid_fade_end=8, grid_percentage=0.1,
                  min_cell_size=4, cell_size=40, max_cell_size=1000,
                  pan_button=MIDDLE_MOUSE, fps=60,
                  allowed_zoom=True, allowed_pan=True, allowed_resize=True):
@@ -65,7 +65,7 @@ class PyGrid:
         self._background_color = background_color
         self._grid_color = grid_color
         self._grid_alpha = 255
-        self._grid_default_alpha = 150
+        self._grid_default_alpha = 255
         self._color_key = (255, 0, 255)
 
         # mouse events are handled once per frame
@@ -263,6 +263,8 @@ class PyGrid:
         self._calc_n_columns()
         self._calc_render_zone()
         self.on_start()
+        self._left_offset = self._grid_thickness // 2
+        self._top_offset = self._grid_thickness // 2
         self._draw_screen()
 
         self._clock = pygame.time.Clock()
@@ -575,8 +577,8 @@ class PyGrid:
         if not self._grid_thickness:
             return
 
-        x = self._cell_size - self._left_offset - self._grid_thickness + self._grid_thickness // 2
-        y = self._cell_size - self._top_offset - self._grid_thickness + self._grid_thickness // 2
+        x = -self._left_offset - self._grid_thickness + self._grid_thickness // 2
+        y = -self._top_offset - self._grid_thickness + self._grid_thickness // 2
 
         # draw grid rows
         for row in range(self._n_rows):
